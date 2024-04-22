9050d4015f375b2e3e8a558574254e317c603112
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2024-04-22 19:24:04 +0200

    Update PetCard.tsx

--- app/component/pets/PetCard.tsx
+++ app/component/pets/PetCard.tsx
@@ -14,7 +14,7 @@ const PetCard : React.FC <PetCardProps> = ({pet}) => {
   const router = useRouter();
 
   return (
-    <div  onClick={() => router.push(`/pet/${pet._id}`)} className="col-span-1 cursor-pointer text- rounded-md border-[1.2px] border-slate-200 bg-gray-900  p-2 transition hover:scale-105 bg-gray-900 text-center text-sm">
+    <div  onClick={() => router.push(`/pet/${pet._id}`)} className="col-span-1 cursor-pointer text- rounded-md border-[1.2px] border-slate-200 text-orange-300  p-2 transition hover:scale-105 bg-gray-900 text-center text-sm">
       
       <div className="flex flex-col items-center w-full gap-1">
       <div className="aspect-square overflow-hidden relative w-full">
