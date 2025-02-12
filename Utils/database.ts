11b7cd9fa57555955bff53f1b980fe2d19203d04
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2025-02-12 20:47:53 +0300

    Update database.ts

--- Utils/database.ts
+++ Utils/database.ts
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
-     if (mongoose.connections[0].readyState) return;
-    try {
-         await mongoose.connect(process.env.DATABASE_URL as string, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-         } as any);
-
-         console.log("Mongo Connection was successful")
-    } catch (error) {
-         throw new Error("Error Connecting to MongoDB.");
+    if (mongoose.connection.readyState === 1) {
+        console.log("Already connected to MongoDB");
+        return;
     }
-}
+
+    try {
+        await mongoose.connect(process.env.DATABASE_URL as string);
+        console.log("Mongo Connection was successful");
+    } catch (error) {
+        console.error("Error Connecting to MongoDB:", error);
+        throw new Error("Error Connecting to MongoDB.");
+    }
+};
 
 export default connect;
